0bc74157b77dbc31906780dbd75407a5c69361a3
Wojciech Czerski <wojciech.czerski@gmail.com>
2021-09-21 23:28:23 +0200

    Initial Commit

---
+++ ParticleEditor/ParticleEditorApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2021 Wojciech Czerski. All rights reserved.
+
+import SwiftUI
+
+@main
+struct ParticleEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
