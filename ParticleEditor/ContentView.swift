db975e042527a3af92f45b3af92e7d880b6abeb1
Wojciech Czerski <wojciech.czerski@gmail.com>
2022-02-21 01:53:07 +0100

    Add dismiss keyboard button

--- ParticleEditor/ContentView.swift
+++ ParticleEditor/ContentView.swift
@@ -286,6 +286,15 @@ struct ContentView: View {
                     }
                 }
             }
+            HStack {
+                Spacer()
+                Button(action: {
+                    let resign = #selector(UIResponder.resignFirstResponder)
+                    UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+                }, label: {
+                    Image(systemName: "keyboard.chevron.compact.down")
+                })
+            }
         }.padding()
     }
 }
